39bf896ad07f6ef5c9fb4ce1ba4d1a0e254c4c09
DevSehhabIbrahem <shehabibrahem222@gmail.com>
2022-08-11 21:43:13 -0700

    parallx

--- src/components/HeroSection/HerSection.jsx
+++ src/components/HeroSection/HerSection.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import { useRef, useEffect } from "react";
 import "./HeroSection.scss";
 import Navbar from "../NavBar/Navbar.jsx";
+import center from "../../asset/symbols@2x.png";
+import Rappid from "../../asset/fighting-rabbits@2x.png";
 const HerSection = () => {
+  const layer = useRef();
+  useEffect(() => {
+    const layerImage = layer.current;
+
+    window.onscroll = () => {
+      const y = window.pageYOffset;
+
+      layerImage.style.transform = "translateY(" + y / -5 + "px)";
+    };
+  }, []);
   return (
     <div className="HerSection">
       <Navbar />
+      <div className="inner__content">
+        <div className="hero__content">
+          <img src={center} alt="center" />
+        </div>
+        <img src={Rappid} alt="Rappid" ref={layer} />
+      </div>
     </div>
   );
 };
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -4,16 +4,18 @@ import logo from "../../asset/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="Navbar flex items-center justify-between max-w-7xl m-auto py-5">
-      <img src={logo} alt="logo" />
-      <div className="flex items-center gap-11">
-        <ul className="flex items-center gap-5">
-          <li>Home</li>
-          <li>Twitter</li>
-          <li>Discord</li>
-          <li>Roadmap</li>
-        </ul>
-        <button>Mint Now</button>
+    <div className="Navbar">
+      <div className="flex items-center justify-between max-w-7xl m-auto py-5">
+        <img src={logo} alt="logo" />
+        <div className="flex items-center gap-11">
+          <ul className="flex items-center gap-5">
+            <li>Home</li>
+            <li>Twitter</li>
+            <li>Discord</li>
+            <li>Roadmap</li>
+          </ul>
+          <button>Mint Now</button>
+        </div>
       </div>
     </div>
   );
